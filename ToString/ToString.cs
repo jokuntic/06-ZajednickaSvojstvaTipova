db6ce7d49de3293bc6afa47b3acd36a7f6162c97
Josip Kuntic <jokuntic@vsite.hr>
2016-11-10 19:44:55 +0100

    Ispravljen Equals za osobu

--- ToString/ToString.cs
+++ ToString/ToString.cs
@@ -3,9 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Diagnostics;
-
+using System.Numerics;
 namespace Vsite.CSharp
 {
+    
     public struct KompleksniBroj
     {
         public KompleksniBroj(double realni, double imaginarni)
@@ -19,9 +20,8 @@ namespace Vsite.CSharp
 
         // TODO: Pregaziti (override) metodu ToString tako da vraća niz u obliku: "2+3j", "2-j", "0", "j", "-j".
         public override string ToString()
-        {
-
-            return string.Format("{0}+{1}j",Realni,Imaginarni);
+        { 
+            return string.Format("{0}+{1}j", Realni, Imaginarni);
         }
 
     }
